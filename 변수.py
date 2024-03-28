24dbd8a94b0520a633737197fe8aa5fe88e7544f
chaneun2 <kchan3534@gmail.com>
2024-03-28 19:43:19 +0900

    로그인 : 카드로 인증 관련 변수 추가

--- 변수.py
+++ 변수.py
@@ -1,5 +1,5 @@
 앱이름='//android.widget.TextView[@content-desc="롯데카드_개발계"]'
-로그인xpath='	/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.Button'
+로그인xpath='//android.widget.Button[@content-desc="로그인 버튼"]'
 로그인id='com.lcacApp:id/btn_login'
 간편번호불일치xpath='/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.Button'
 간편번호불일치Id='com.lcacApp:id/bt_loca_confirm'
@@ -15,6 +15,28 @@
 시작하기_로그인Id='com.lcacApp:id/bt_tutorial_login'
 시작하기_로그인xpath='//android.widget.Button[@content-desc="로그인 버튼"]'
 
+로그인= '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.Button'
+다른로그인방법= '//android.widget.TextView[@content-desc="다른 로그인 방법 버튼"]'
+카드로인증= '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.TextView[2]'
+
+#네이티브비번
+비번xpath0= '//android.widget.ImageView[@content-desc="0"]'
+비번xpath1= '//android.widget.ImageView[@content-desc="1"]'
+비번xpath2= '//android.widget.ImageView[@content-desc="2"]'
+비번xpath3= '//android.widget.ImageView[@content-desc="3"]'
+비번xpath4= '//android.widget.ImageView[@content-desc="4"]'
+비번xpath5= '//android.widget.ImageView[@content-desc="5"]'
+비번xpath6= '//android.widget.ImageView[@content-desc="6"]'
+비번xpath7= '//android.widget.ImageView[@content-desc="7"]'
+비번xpath8= '//android.widget.ImageView[@content-desc="8"]'
+비번xpath9= '//android.widget.ImageView[@content-desc="9"]'
+
+
+#카드유효기간
+유효기간xpath= '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout[2]/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.EditText'
+유효기간id= 'com.lcacApp:id/et_mobile_phone_auth_front_resident_registration_number'
+
+#테스트계정정보
 고객명='안추도'
 생년월일='920124'
 주민뒷번호='1'
@@ -22,3 +44,5 @@
 카드유효기간='0125'
 카드cvc='955'
 
+#지문 관련 화면 및 버튼
+지문닫기= '//android.widget.ImageButton[@content-desc="닫기"]'
