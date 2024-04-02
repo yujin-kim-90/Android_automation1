bb0ef1c399a18a62a8d86919a917b4bce813dbd0
yujin-kim-90 <aaaqwww149@naver.com>
2024-04-02 19:41:33 +0900

    tcid: log_3,4,5

--- test.py
+++
@@ -1,3 +0,0 @@
-import OpenCV
-
-print (OpenCV.test())
--- 로그인_유진.py
+++ 로그인_유진.py
@@ -30,13 +30,14 @@ currentPath = '%s/' % os.getcwd()
 screenshotPath = currentPath + '%s-screenshot.png'
 
 
-# tc_id: log_03
+# tc_id: log_03, log_04
 def test_case_01(driver)->None:
     driver.find_element(By.XPATH, 변수.앱이름).click()
     time.sleep(10)
-    driver.find_element(By.ID, 변수.로그인id).click()
-    time.sleep(3)
+    #driver.find_element(By.ID, 변수.로그인id).click()
+    #time.sleep(3)
     driver.save_screenshot(screenshotPath)
+    j=1
     for i in range(5):
         for i in range(3):
             name = 'card_setting_1.png'
@@ -44,12 +45,59 @@ def test_case_01(driver)->None:
             name = 'card_setting_2.png'
             driver.tap([OpenCV.Matching.detectimage(name)])
             time.sleep(2)
-        j=1
+        str1=driver.find_element(By.ID, 변수_로그인.간편번호불일치text_id).text
         if j <3:
-            As
-            assert driver.find_element(By.ID, 변수_로그인.간편번호불일치text_id).text == '간편번호가 일치하지 않습니다.('+j+'/5)'
+            str2="간편번호가 일치하지 않습니다.("+str(j)+"/5)"
+            j+=1
+            assert str1 == str2
         elif 3<=j<5:
-            assert driver.find_element(By.ID, 변수_로그인.간편번호불일치text_id).text == '간편번호가 일치하지 않습니다.(' + j+ '/5) 5회 연속 잘못 입력 시 이용이 제한됩니다.'
+            str2="간편번호가 일치하지 않습니다.(" + str(j)+ "/5)\n5회 연속 잘못 입력 시 이용이 제한됩니다."
+            j+=1
+            assert str1 == str2
         else:
-            assert driver.find_element(By.ID,변수_로그인.간편번호불일치text_id).text == '간편번호를 연속 5회 잘못 입력으로 이용할 수 없습니다. 본인인증 후 간편번호를 재설정해 주세요.'
+            str2="간편번호를 연속 5회 잘못 입력으로\n이용할 수 없습니다.\n 본인인증 후 간편번호를 \n재설정해 주세요."
+            j+=1
+            assert str1 == str2
         driver.find_element(By.ID, 변수_로그인.간편번호불일치_확인_id).click()
+
+#tc_id: log_05
+def test_case_02(driver)->None:
+    driver.find_element(By.ID, 변수_로그인.간편번호재설정버튼_id).click()
+    time.sleep(5)
+    str1=driver.find_element(By.ID, 변수_로그인.간편번호재설정_본인인증_타이틀_id).text
+    assert str1 == '간편번호 설정'
+
+#tc_id: log_06
+def test_case_03(driver)->None:
+    driver.find_element(By.XPATH, 변수.앱이름).click()
+    driver.find_element(By.ID, 변수_로그인.간편번호재설정_이름_id).send_keys(변수.고객명)
+    driver.find_element(By.ID, 변수_로그인.간편번호재설정_주민등록번호_id).send_keys(변수.생년월일)
+    driver.find_element(By.ID, 변수_로그인.간편번호재설정_주민뒷번호_id).click()
+    time.sleep(3)
+    # opencv 이용할 때, 스크린샷 한번해주고 , 파일명 선언 필요해요
+    driver.save_screenshot(screenshotPath)
+    name = '1_keypad_login.png'
+    driver.tap([OpenCV.Matching.detectimage(name)])
+
+    driver.find_element(By.XPATH, 변수_로그인.간편번호재설정_통신사).click()
+    driver.find_element(By.ID, 변수_로그인.간편번호재설정_인증요청).click()
+    time.sleep(5)
+    driver.find_element(By.ID, 변수_로그인.간편번호재설정_전체동의하고인증id).click()
+    time.sleep(10)
+    driver.find_element(By.ID, 변수_로그인.간편번호재설정_인증번호발송_id).click()
+    driver.find_element(By.ID, 변수.로그인_간편번호_인증번호입력id).send_keys("111111")
+    time.sleep(10)
+
+    for i in range(2):
+        driver.save_screenshot(screenshotPath)
+        for i in range(3):
+            name= 'card_setting_1.png'
+            driver.tap([OpenCV.Matching.detectimage(name)])
+            name = 'card_setting_4.png'
+            driver.tap([OpenCV.Matching.detectimage(name)])
+            time.sleep(2)
+
+    assert driver.find_element(By.ID, 변수_로그인.간편번호재설정_확인문구id).text == "간편번호가 설정되었습니다."
+def test_case_04(driver)->None:
+    driver.find_element(By.ID, 변수_로그인.간편번호재설정_설정확인id).click()
+    time.sleep(10)
